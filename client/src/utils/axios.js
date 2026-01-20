6586d659f93d3dc4fa986ee4dd90c126e23696a8
Samit Sharma <samisharma501@gmail.com>
2026-01-20 12:28:09 +0530

    update backend api url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://YOUR-BACKEND.onrender.com",
+  baseURL: "https://netflix-repo.onrender.com",
   withCredentials: true
 });
 
