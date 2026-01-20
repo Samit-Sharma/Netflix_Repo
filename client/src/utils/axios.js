64f0fd164b9552c1f5036683cd7522f7aed1828e
Samit Sharma <samisharma501@gmail.com>
2026-01-20 14:35:56 +0530

    Add 60s timeout to axios for Render backend wake-up delay

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,8 @@
 
 const api = axios.create({
   baseURL: "https://netflix-repo.onrender.com",
-  withCredentials: true
+  withCredentials: true,
+  timeout: 60000 // 60 second timeout for slow backend wake-up
 });
 
 export default api;
