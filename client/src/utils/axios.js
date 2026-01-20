bb3693cebec95ea98b6b2cd150e713a1df8d59bd
Samit Sharma <samisharma501@gmail.com>
2026-01-20 14:30:35 +0530

    Uncomment axios configuration

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,8 @@
 // import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: "https://netflix-repo.onrender.com",
-//   withCredentials: true
-// });
+const api = axios.create({
+  baseURL: "https://netflix-repo.onrender.com",
+  withCredentials: true
+});
 
-// export default api;
+export default api;
